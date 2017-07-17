a0a6dede4ae8ee632223d013840ae1828e4f5f01
nshhhin <cs172004@meiji.ac.jp>
2017-07-17 16:22:02 +0900

    update file

--- getMoodFromLyrics.py
+++ getMoodFromLyrics.py
@@ -5,6 +5,7 @@
 from bs4 import BeautifulSoup
 import urllib.request
 from urllib.request import Request, urlopen
+import sys
 
 import re #正規表現ライブラリ
  
@@ -12,28 +13,37 @@ import re #正規表現ライブラリ
 #url = 'https://www5.atwiki.jp/hmiku/pages/249.html'
 
 
+for i in range(1,2):
 
-for()
-url = "http://utaten.com/lyricPvRanking/index?page=4"
+	url = "http://utaten.com/lyricPvRanking/index?page="+str(i)
 
-# スクレイピングできないページもあるので，FireFoxでアクセスする 
+	# スクレイピングできないページもあるので，FireFoxでアクセスする 
 
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-response = urlopen(req)
-html = response.read()
-soup = BeautifulSoup(html, "lxml")
+	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	response = urlopen(req)
+	html = response.read()
+	soup = BeautifulSoup(html, "lxml")
 
-# {曲名:歌詞が載っているURL}の辞書song_dictを作る
-song_dict =  {}
+	# {曲名:歌詞が載っているURL}の辞書song_dictを作る
+	song_dict =  {}
 
-for h3 in soup.find_all("h3"):
-	#print(h3.a.get_text())
-	text = h3.a.get_text()
-	split_text = text.split("        ")
-	print(split_text[1])
+	for h3 in soup.find_all("h3"):
+		#print(h3.a.get_text())
+		try:
+			text = h3.a.get_text()
+			split_text = text.split("        ")
+			song_title = split_text[1]
+			song_url = "http://utaten.com" + h3.a.get("href")
+			song_dict.update({song_title, song_url})
+
+		except:
+			#なんもしない
+			print(sys.exc_info())
+
 
 
 """
+# wikiから持ってくるバージョン
 # 曲リストを取得してくる
 for initial_word in soup.find_all("div", class_="plugin_list_by_tag"):
 	for song in initial_word.ul.find_all("li"):
@@ -44,10 +54,3 @@ for initial_word in soup.find_all("div", class_="plugin_list_by_tag"):
 print(song_dict["結晶"])
 
 """
-'''
-<div class="plugin_list_by_tag">
-<ul>
-~~~~ ←ここをもってくる
-</ul>
-</div>
-'''
